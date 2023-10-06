9a561b5ae9309c35c9f28766e432a61c43a3a0d0
jy-hwang <stratojy@gmail.com>
2023-10-06 17:32:07 +0900

    #6 - 오탈자 수정
    임시 테스트 계정은 4개.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- 테스트계정 5개
+-- 테스트계정 4개
 -- TODO : 테스트용 이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을지 고민해봐야함. -> 시큐리티 적용하려면 어떻게 해야할지 조금 고민해봐야할 문제.
 insert into user_account(user_id, user_password,role_types, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
   ('admin1','{noop}asdasd1234','ADMIN','admin1','admin1@abc.com','I am your father', now(), 'admin1',now(),'admin1')
