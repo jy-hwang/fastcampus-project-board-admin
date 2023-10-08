154251a40c9d947f094ce643f858bae9d94a80cd
jy-hwang <stratojy@gmail.com>
2023-10-08 19:01:59 +0900

    #16 - 회원 관리 비즈니스 로직 및 컨트롤러 테스트 작성
    어드민 서비스에서 `UserAccount` 도메인에는 더 이상 roleType 이 필요 없으므로 지워주고
    필요부분 수정해줌.

--- src/test/java/com/fastcampus/projectboardadmin/service/ArticleManagementServiceTest.java
+++ src/test/java/com/fastcampus/projectboardadmin/service/ArticleManagementServiceTest.java
@@ -168,7 +168,6 @@ class ArticleManagementServiceTest {
     private UserAccountDto createUserAccountDto() {
       return UserAccountDto.of(
           "jackieHwang",
-          Set.of(RoleType.ADMIN),
           "jackieHwang@abc.com",
           "jackieHwang",
           "IamWeb-Programmer"
