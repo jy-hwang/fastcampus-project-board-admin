7c8e4b1942846c797d4722ae0c240fc711d472d8
jy-hwang <stratojy@gmail.com>
2023-10-11 16:02:04 +0900

    #21 - 어드민 회원 기능, 컨트롤러, 뷰 구현
    jsGrid 기능을 이용하여 뷰 구현하였음.
    이번 프로젝트는 백엔드 기술에 최대한 집중하려고 하지만
    아주 조금씩 맛보기 정도로 프론트엔드 기술을 사용하고 있음.

--- src/main/java/com/fastcampus/projectboardadmin/service/AdminAccountService.java
+++ src/main/java/com/fastcampus/projectboardadmin/service/AdminAccountService.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboardadmin.service;
 
+import com.fastcampus.projectboardadmin.domain.AdminAccount;
 import com.fastcampus.projectboardadmin.domain.constant.RoleType;
 import com.fastcampus.projectboardadmin.dto.AdminAccountDto;
 import com.fastcampus.projectboardadmin.repository.AdminAccountRepository;
@@ -8,16 +9,20 @@ import java.util.Optional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class AdminAccountService {
 
 
   private final AdminAccountRepository adminAccountRepository;
 
+  @Transactional(readOnly = true)
   public Optional<AdminAccountDto> searchUser(String username) {
-    return Optional.empty();
+    return adminAccountRepository.findById(username)
+        .map(AdminAccountDto::from);
   }
 
   public AdminAccountDto saveUser(
@@ -27,15 +32,21 @@ public class AdminAccountService {
       String email,
       String nickname,
       String memo) {
-    return null;
+    return AdminAccountDto.from(
+        adminAccountRepository.save(
+            AdminAccount.of(username, password, roleTypes, email, nickname, memo)));
   }
 
+  @Transactional(readOnly = true)
   public List<AdminAccountDto> users() {
-    return List.of();
+    return adminAccountRepository.findAll()
+        .stream()
+        .map(AdminAccountDto::from)
+        .toList();
   }
 
   public void deleteUser(String username) {
-
+    adminAccountRepository.deleteById(username);
   }
 
 }
